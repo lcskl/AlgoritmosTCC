c3b61744ce6da9abfa651f22f32101b6cc8d1fa1
Lucas <lucas.keiler1984@gmail.com>
2019-03-23 11:49:03 -0300

    WIP: Transform Function in Solid Grid Finished

--- solidGrid.cpp
+++ solidGrid.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 using namespace std;
 
+#define INF 999999999
 
 
 class Graph{
@@ -22,6 +23,34 @@ public:
 
         visited = new int[n+1];
         degree  = new int[n+1];
+    }
+
+    int d_func(int b, int k){
+    	if(b == 0)
+    		return ( (k-1) +  int((2*(k+1))/4) );
+    	else
+    		return (   k  +   int((2*(k-1))/4) );
+    }
+
+    int calculateDistance(int other1,int other2,int b,int k){
+    	int deg_sum = degree[other1] + degree[other2];
+    	int weight = INF;
+    	switch(deg_sum){
+    		case 6:
+    			weight = d_func(b,k);
+    			break;
+    		case 5:
+    			if(k > 1)
+    				weight = d_func(b,k-1);
+    			break;
+    		case 4:
+    			if(k > 2)
+    				weight = d_func(b,k-2);
+    			else
+    				weight = -INF;
+    			break; 
+    	}
+    	return weight;
     }
 
 
@@ -63,7 +92,7 @@ public:
     void Transform(){
     	int k; //Ladder L_k
     	for(auto &kv : Ladder){
-    		k = kv.second.size() / 2;
+    		k = (kv.second.size() / 2) - 1;
 
     		vector<int> corner_vertex; //It is necessary to know the corner vertices
     		for(auto vertex : kv.second){
@@ -96,11 +125,84 @@ public:
     			}
     		}
 
-    		for(int i=0;i<n;i++)
-    			cout << i << " -> " << BFSvisited[i] << endl;
+    		/*
+				Now we can determine which vertices are adjacent, at a distance k apart, and at a distance k+1 apart.
+				Below, the variables A,B,C,D will represent the topology:
+
+					A ---- ... ---- C
+					|               |
+					|               |
+					B ---- ... ---- D
+    		*/
+
+    		int A,B = -1,C,D;
+    		A = corner_vertex[0];
 
 
-    		//To-do: BFS em Ladder
+    		for(auto corner : corner_vertex){
+    			if(corner == A)continue;
+
+    			if (B == -1 && find(adjList[A].begin(),adjList[A].end(),make_pair(corner,1)) != adjList[A].end()) //'corner' is adjacent -> That's B
+    				B = corner;
+    			else if(BFSvisited[corner] == k) // 'corner' is at distance k from A -> That's C
+    				C = corner;
+    			else if(BFSvisited[corner] == k+1) // 'corner' is at distance k+1 from A -> That's D
+    				D = corner;
+    			
+    			cout << corner << " -> " << BFSvisited[corner] << endl;
+    		}
+
+    		cout << A << " " << B << " " << C << " " << D << "\n";
+
+    		/* We need to define the following distances:
+			   A to C
+			   B to D
+			   A to D
+			   B to C 
+			*/
+
+			//First, distance K (A,C) & (B,D)
+			int distance = calculateDistance(B,D,0,k);
+			if(k > 1 && distance != INF){
+				adjList[A].push_back(make_pair(C,distance));
+				adjList[C].push_back(make_pair(A,distance));
+			}
+			
+
+			distance = calculateDistance(A,C,0,k);
+			if(k > 1 && distance != INF){
+				adjList[B].push_back(make_pair(D,distance));
+				adjList[D].push_back(make_pair(B,distance));
+			}
+			
+			//Now, distance K+1 (A,D) & (B,C)
+			distance = calculateDistance(B,C,1,k);
+			if(distance != INF){
+				adjList[A].push_back(make_pair(D,distance));
+				adjList[D].push_back(make_pair(A,distance));
+			}
+
+			distance = calculateDistance(A,D,1,k);
+			if(distance != INF){
+				adjList[B].push_back(make_pair(C,distance));
+				adjList[C].push_back(make_pair(B,distance));
+			}
+
+			//Now, delete remaining vertices of the ladder
+			for(auto vertex : kv.second){
+				if(find(corner_vertex.begin(),corner_vertex.end(),vertex) == corner_vertex.end() ){
+					adjList[vertex].clear();
+					continue;
+				}
+
+    			for(int i=0; i < adjList[vertex].size();i++){
+    				if(find(corner_vertex.begin(),corner_vertex.end(),adjList[vertex][i].first) == corner_vertex.end()){
+    					adjList[vertex].erase(adjList[vertex].begin()+i);
+    					i--;
+    				}
+    			}
+    		}
+    		
     	}
     }
 };
@@ -142,6 +244,14 @@ int main(int argc, char const *argv[])
     }
 
     graph.Transform();
+
+    for(int i=0;i<n_vertex;i++){
+    	cout << i << " => ";
+    	for(int j=0;j<graph.adjList[i].size();j++){
+    		cout << "(" << graph.adjList[i][j].first << " ," << graph.adjList[i][j].second << ") ";
+    	}
+    	cout << endl;
+    }
 
 
 	return 0;
