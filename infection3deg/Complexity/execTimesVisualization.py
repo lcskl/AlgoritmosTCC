96fee646038cdb7a1ed100ac782ab3017445fd26
lucaskeiler <lucas.keiler1984@gmail.com>
2019-09-17 09:47:47 -0300

    Finish Complexity

--- infection3deg/Complexity/execTimesVisualization.py
+++ infection3deg/Complexity/execTimesVisualization.py
@@ -28,18 +28,37 @@ yInterp = f(xInterp)
 
 #### O(n^2) Execution Time GuideLine #######################################
 x2,y2 = loadTimeFile('complexityGuideOn2.txt')
-f = interpolate.interp1d(x,y,kind='quadratic',fill_value="extrapolate")
+f2 = interpolate.interp1d(x2,y2,kind='quadratic',fill_value="extrapolate")
 
-xInterp = np.arange(0,max(x))
-yInterp = f(xInterp)
+xInterp_n2 = np.arange(0,max(x2))
+yInterp_n2 = f2(xInterp_n2)
+#############################################################################
+
+#### O(n^2) Execution Time GuideLine #######################################
+x3,y3 = loadTimeFile('complexityGuideOn3.txt')
+f3 = interpolate.interp1d(x3,y3,kind='quadratic',fill_value="extrapolate")
+
+m = -1
+for i in np.arange(0,max(x),10):
+    if( f3(i) > max(y) ):
+        m = i
+        break
+
+xInterp_n3 = np.arange(0,m)
+yInterp_n3 = f3(xInterp_n3)
 #############################################################################
 
 
 plt.scatter(x, y, edgecolors='r')
-plt.plot(x2,y2,'r-')
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-plt.plot(xInterp,yInterp)
-plt.xlabel('Vertex Set Size')
-plt.ylabel('Execution Time (s)')
+
+algorithmLine, = plt.plot(xInterp,yInterp, label='Algorithm')
+guide_On2, = plt.plot(xInterp_n2,yInterp_n2, label='Guide Curve $O(n^2)$')
+guide_On3, = plt.plot(xInterp_n3, yInterp_n3, label='Guide Curve $O(n^3)$')
+
+plt.xlabel('Vertex Set Size', labelpad=7)
+plt.ylabel('Execution Time (s)', labelpad=7)
 plt.title('Complexity Test')
+plt.legend(handles=[algorithmLine,guide_On2,guide_On3])
+plt.savefig("complexity.svg")
 plt.show()
