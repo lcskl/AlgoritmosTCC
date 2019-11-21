1a0698aa63070b0fbedb9656e2628b15e1ab64c8
lucaskeiler <lucas.keiler1984@gmail.com>
2019-11-21 12:38:43 -0300

    complexity tree t(G)

---
+++ tree_t(G)/Complexity/execTimesVisualization.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+def loadTimeFile(fileName):
+    timeList = []
+    verticesList = []
+    with open(fileName) as file:
+        line = file.readline()
+        while line:
+            s = line.split(' ')
+            n = int(s[0])
+            time = float(s[1])
+            verticesList.append(n)
+            timeList.append(time)
+
+            line = file.readline()
+    return verticesList, timeList
+
+#################### Execution Data and Interpolation ######################
+x,y = loadTimeFile('timeReport.txt')
+f = interpolate.interp1d(x,y,kind='quadratic',fill_value="extrapolate")
+
+xInterp = np.arange(0,max(x))
+yInterp = f(xInterp)
+
+############################################################################
+
+#### O(n) Execution Time GuideLine #######################################
+x2,y2 = loadTimeFile('complexityGuide_n.txt')
+f2 = interpolate.interp1d(x2,y2,kind='quadratic',fill_value="extrapolate")
+
+xInterp_n2 = np.arange(0,max(x))
+yInterp_n2 = f2(xInterp_n2)
+#############################################################################
+
+#### O(n.logn) Execution Time GuideLine #######################################
+x3,y3 = loadTimeFile('complexityGuide_nlogn.txt')
+f3 = interpolate.interp1d(x3,y3,kind='quadratic',fill_value="extrapolate")
+
+xInterp_n3 = np.arange(0,max(x))
+yInterp_n3 = f3(xInterp_n3)
+#############################################################################
+
+
+plt.scatter(x, y, edgecolors='r')
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+algorithmLine, = plt.plot(xInterp,yInterp, label='Algorithm: t(G) in Trees')
+guide_On2, = plt.plot(xInterp_n2,yInterp_n2, label='Guide Curve $O(n)$')
+guide_On3, = plt.plot(xInterp_n3, yInterp_n3, label='Guide Curve $O(n log(n))$')
+
+plt.xlabel('Vertex Set Size', labelpad=7)
+plt.ylabel('Execution Time (s)', labelpad=7)
+plt.title('Complexity Test')
+plt.legend(handles=[algorithmLine,guide_On2,guide_On3])
+plt.savefig("complexity.svg")
+plt.show()
---
+++ tree_t(G)/Complexity/tree.hpp
@@ -0,0 +1,31 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+#include <vector>
+
+class Graph{
+public:
+    int n;
+    int** s;
+    std::vector< std::vector<int> > adjList;
+    std::vector< int > inf_time;
+    std::vector< int > neighbors_remaining;
+    std::vector< int > max_neighbor_time;
+    std::vector< int > perc_limit; 
+
+    Graph(int const& n_vertex);
+
+    int max_time();
+
+    int max_time_arbitrary();
+
+    void add_edge(int a,int b);
+
+    void remove_edge(int a,int b);
+
+private:
+    int calculate(int u,int v);
+};
+
+
+#endif
